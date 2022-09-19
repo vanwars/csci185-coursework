3ab763342ecea5caab8c92320bfeee6d3eb3b245
Sarah <vanwars@gmail.com>
2022-09-19 11:15:17 -0400

    updated homepage

---
+++ tutorials/tutorial06/01-font-switcher/index.js
@@ -0,0 +1,18 @@
+const makeBigger = () => {
+   alert('make bigger!');
+};
+
+const makeSmaller = () => {
+   alert('make smaller!');
+};
+
+/*
+Tips:
+1. First, in the index.html file, add an onclick attribute to each button.
+   The value of the attribute should be a call to the corresponding function
+   (see class demos).
+
+2. Then modify the body of the "makeBigger" and 
+   "makeSmaller" functions (in between the curly braces)
+   to target the body element and set it's font size.
+*/
