b595b1e142050c48ffe2ac9a4bd6fe36f0d766a4
Sarah <vanwars@gmail.com>
2022-09-25 17:20:21 -0400

    updates

--- tutorials/tutorial06/01-font-switcher/index.js
+++ tutorials/tutorial06/01-font-switcher/index.js
@@ -1,18 +1,9 @@
-const makeBigger = () => {
-   alert('make bigger!');
+const makeBigger = (ev) => {
+    document.querySelector('.content').style.fontSize = '3em';
+    document.querySelector('h1').style.fontSize = '3em';
 };
 
-const makeSmaller = () => {
-   alert('make smaller!');
+const makeSmaller = (ev) => {
+    document.querySelector('.content').style.fontSize = '1.4em';
+    document.querySelector('h1').style.fontSize = '2em';
 };
-
-/*
-Tips:
-1. First, in the index.html file, add an onclick attribute to each button.
-   The value of the attribute should be a call to the corresponding function
-   (see class demos).
-
-2. Then modify the body of the "makeBigger" and 
-   "makeSmaller" functions (in between the curly braces)
-   to target the body element and set it's font size.
-*/
--- tutorials/tutorial06/02-theme-switcher/index.js
+++ tutorials/tutorial06/02-theme-switcher/index.js
@@ -1,25 +1,21 @@
 const defaultTheme = ev => {
-    // your code here.
+    document.querySelector('body').className = '';
 };
 
 const oceanTheme = ev => {
-   // your code here.
+   document.querySelector('body').className = 'ocean';
 };
 
 const desertTheme = ev => {
-   // your code here.
+   document.querySelector('body').className = 'desert';
 };
 
 const highContrastTheme = ev => {
-    // your code here.
+    document.querySelector('body').className = 'high-contrast';
 }; 
 
-/*
-    Hints: 
-    1. Attach the event handlers (functions) above to the click event
-       of each of the four buttons (#default, #ocean, #desert, 
-        and #high-contrast) in index.html.
-    2. Then, modify the  body of each function so that it
-       sets the className property of the body tag based on 
-       the button that was clicked.
-*/
+
+document.querySelector('#default').addEventListener('click', defaultTheme); 
+document.querySelector('#ocean').addEventListener('click', oceanTheme); 
+document.querySelector('#desert').addEventListener('click', desertTheme); 
+document.querySelector('#high-contrast').addEventListener('click', highContrastTheme); 
